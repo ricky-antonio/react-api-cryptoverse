76284aa1776358ab44e82cdad31e5b0a4c1b2f00
Ricardo Monterrosa <rickyantonio117@gmail.com>
2022-01-29 11:37:16 -0800

    create routes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 
 import React from 'react';
-import { Switch, Route, Link } from 'react-router-dom';
-import { layout, Typography, Space } from 'antd';
+import { Routes, Route, Link } from 'react-router-dom';
+import { Layout, Typography, Space } from 'antd';
 
-import { Navbar } from './components';
+import { Navbar, Exchanges, Homepage, Cryptocurrencies, News, CryptoDetails } from './components';
 
 import './App.css';
 
@@ -14,10 +14,20 @@ function App() {
 				<Navbar />
 			</div>
 			<div className="main">
-
+				<Layout>
+					<div className="routes">
+						<Routes>
+							<Route exact path="/" element={<Homepage />} />
+							<Route exact path="/exchanges" element={<Exchanges />} />
+							<Route exact path="/cryptocurrencies" element={<Cryptocurrencies />} />
+							<Route exact path="/cryto/:coinId" element={<CryptoDetails />} />
+							<Route exact path="/news" element={<News />} />
+						</Routes>
+					</div>
+				</Layout>
 			</div>
 			<div className="footer">
-				
+
 			</div>
 		</div>
 	);
---
+++ src/components/Homepage.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Homepage = () => {
+  return <div>homne</div>;
+};
+
+export default Homepage;
