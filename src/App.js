9acd82ba714ae2180c64db6028c9e57f5dfa586a
Ricardo Monterrosa <rickyantonio117@gmail.com>
2022-01-29 11:51:48 -0800

    add footer

--- src/App.js
+++ src/App.js
@@ -20,15 +20,24 @@ function App() {
 							<Route exact path="/" element={<Homepage />} />
 							<Route exact path="/exchanges" element={<Exchanges />} />
 							<Route exact path="/cryptocurrencies" element={<Cryptocurrencies />} />
-							<Route exact path="/cryto/:coinId" element={<CryptoDetails />} />
+							<Route exact path="/crypto/:coinId" element={<CryptoDetails />} />
 							<Route exact path="/news" element={<News />} />
 						</Routes>
 					</div>
 				</Layout>
+				<div className="footer">
+					<Typography.Title level={5} style={{ color: '#fff', textAlign: 'center' }} >
+						Cryptoverse <br />
+						All rights reserved
+					</Typography.Title>
+					<Space>
+						<Link to="/">Home</Link>
+						<Link to="/exchanges">Exchanges</Link>
+						<Link to="/news">News</Link>
+					</Space>
+				</div>
 			</div>
-			<div className="footer">
 
-			</div>
 		</div>
 	);
 }
